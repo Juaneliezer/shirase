16891306c2938bc8c73e26488adf96b0f43b52ea
Juaneliezer <jeliezer26@hotmail.com>
2024-10-30 01:39:26 -0400

    setting the login window

--- login.cpp
+++ login.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include <ncurses.h>
 #include <string>
+using namespace std;
 void login() {
   int starx, starty;
-  WINDOW *winlog = newwin(474, 264, 5, 5);
+  WINDOW *winlog = newwin(15, 50, 17, 67);
   wborder(winlog, '|', '|', '-', '-', '#', '+', '+', '+');
+  mvwprintw(winlog, 0, 22, "Log In");
+  mvwprintw(winlog, 2, 3, "Username\n");
+  char User[80];
+  wgetstr(winlog, User);
   wrefresh(winlog);
 }

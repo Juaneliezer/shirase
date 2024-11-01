0d86391e12218def6004fa1c3339d82573ad6708
Juaneliezer <Jeliezer26@hotmail.com>
2024-11-01 00:48:30 -0400

    coments

--- login.cpp
+++ login.cpp
@@ -1,18 +1,20 @@
 #include <ncurses.h>
 void login() {
-  int starx, starty;
+  int starx, starty; // i dont know why i put this variables
 
+  // setting the space for the window login
   WINDOW *winlog = newwin(15, 50, 17, 67);
-  start_color();
+  start_color(); // maybe i will have to change this
   init_pair(1, COLOR_BLACK, COLOR_WHITE);
   // wattron(winlog, A_STANDOUT | A_UNDERLINE | A_INVIS);
   // wattrset(winlog, 1);
   // wattron(winlog, COLOR_PAIR(1));
 
+  // setting the drawing of the window
   box(winlog, 0, 0);
   wbkgd(winlog, COLOR_PAIR(1));
 
-  // COLOR_PAIR(1);
+  // setting window objects
   mvwprintw(winlog, 0, 22, "Log In");
   mvwprintw(winlog, 2, 3, "Username\n");
   mvwprintw(winlog, 3, 3, "|");
@@ -23,8 +25,9 @@ void login() {
   mvwprintw(winlog, 8, 3, "|");
   char *key = new char[1500];
   wgetstr(winlog, key);
-  wrefresh(winlog);
+  // finishing the window login
   getch();
+  wrefresh(winlog);
   delwin(winlog);
   delete[] key;
 }

b8318572d23e2f96d9c51cc735e21a46852520c3
Juaneliezer <Jeliezer26@hotmail.com>
2024-11-14 20:10:46 -0400

    nothing

--- src/main_window/wtop.cpp
+++ src/main_window/wtop.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <ncurses.h>
 
-using namespace std;
+void toptions();
 void wtop() {
   // init the top window
   WINDOW *wtop = newwin(2, 187, 0, 0);
@@ -12,6 +11,23 @@ void wtop() {
   mvwprintw(wtop, 1, 0, "F1:Watching");
   mvwprintw(wtop, 1, 15, "F2:To watch");
   mvwprintw(wtop, 1, 30, "F3:Dropped");
+  // funoptions();
+
+  int chtop = wgetch(wtop);
+  switch (chtop) {
+  default:
+  case KEY_F(1):
+    // watching();
+    break;
+
+  case KEY_F(2):
+    // watch();
+    break;
+
+  case KEY_F(3):
+    // dropped();
+    break;
+  }
 
   wrefresh(wtop);
 }
